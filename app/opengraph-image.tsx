f8d517cab5451bb2f2ce5d019fbb188d8997ef4f
Frehitzz <fritzharlydegamo@gmail.com>
2026-03-30 11:45:08 +0800

    changing image when sharing the website

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,12 +1,11 @@
 import { readFile } from "node:fs/promises";
 import path from "node:path";
-import { ImageResponse } from "next/og";
 
-export const alt = "iPay business payments preview";
+export const alt = "iPay logo";
 
 export const size = {
-  width: 1200,
-  height: 630,
+  width: 128,
+  height: 39,
 };
 
 export const contentType = "image/png";
@@ -15,58 +14,11 @@ export default async function OpenGraphImage() {
   const logoBuffer = await readFile(
     path.join(process.cwd(), "public", "ipaylogo-white.png")
   );
-  const logoSrc = `data:image/png;base64,${logoBuffer.toString("base64")}`;
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "#0d0d1a",
-          position: "relative",
-          fontFamily: "sans-serif",
-        }}
-      >
-        <div
-          style={{
-            position: "absolute",
-            width: 520,
-            height: 520,
-            borderRadius: 9999,
-            background:
-              "radial-gradient(circle, rgba(245, 158, 11, 0.22) 0%, rgba(245, 158, 11, 0) 68%)",
-          }}
-        />
-
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-            padding: "64px",
-            zIndex: 1,
-          }}
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={logoSrc}
-            alt="iPay logo"
-            width="760"
-            height="232"
-            style={{
-              display: "flex",
-              objectFit: "contain",
-            }}
-          />
-        </div>
-      </div>
-    ),
-    size
-  );
+  return new Response(new Uint8Array(logoBuffer), {
+    headers: {
+      "Content-Type": contentType,
+      "Cache-Control": "public, immutable, no-transform, max-age=31536000",
+    },
+  });
 }
